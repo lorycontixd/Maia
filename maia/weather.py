d8eb382d5a291844576981832e23939f20fc3407
Lorenzo <loryconti1@gmail.com>
2023-09-30 09:39:44 +0200

    Removed test index

---
+++ maia/weather.py
@@ -0,0 +1,9 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+from maia.auth import login_required
+from maia.db import get_db
+
+bp = Blueprint('weather', __name__)
+
